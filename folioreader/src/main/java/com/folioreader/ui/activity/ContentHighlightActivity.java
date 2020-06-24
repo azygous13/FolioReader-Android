8a84b3a85e1f0425a47652b7d01c277959dea1a6
Teerapong Chantakard <azygous13@gmail.com>
2020-06-24 11:11:50 +0700

    Bring highlight feature back

--- folioreader/src/main/java/com/folioreader/ui/activity/ContentHighlightActivity.java
+++ folioreader/src/main/java/com/folioreader/ui/activity/ContentHighlightActivity.java
@@ -44,17 +44,22 @@ public class ContentHighlightActivity extends AppCompatActivity {
     private void initViews() {
 
         UiUtil.setColorIntToDrawable(mConfig.getThemeColor(), ((ImageView) findViewById(R.id.btn_close)).getDrawable());
+        findViewById(R.id.layout_content_highlights).setBackgroundDrawable(UiUtil.getShapeDrawable(mConfig.getThemeColor()));
 
         if (mIsNightMode) {
             findViewById(R.id.toolbar).setBackgroundColor(Color.BLACK);
             findViewById(R.id.btn_contents).setBackgroundDrawable(UiUtil.createStateDrawable(mConfig.getThemeColor(), ContextCompat.getColor(this, R.color.black)));
+            findViewById(R.id.btn_highlights).setBackgroundDrawable(UiUtil.createStateDrawable(mConfig.getThemeColor(), ContextCompat.getColor(this, R.color.black)));
             ((TextView) findViewById(R.id.btn_contents)).setTextColor(UiUtil.getColorList(ContextCompat.getColor(this, R.color.black), mConfig.getThemeColor()));
+            ((TextView) findViewById(R.id.btn_highlights)).setTextColor(UiUtil.getColorList(ContextCompat.getColor(this, R.color.black), mConfig.getThemeColor()));
 
         } else {
             findViewById(R.id.toolbar).setBackgroundColor(Color.BLACK);
-            ((TextView) findViewById(R.id.btn_contents)).setTextColor(mConfig.getThemeColor());
-//            ((TextView) findViewById(R.id.btn_contents)).setTextColor(UiUtil.getColorList(ContextCompat.getColor(this, R.color.white), mConfig.getThemeColor()));
-//            findViewById(R.id.btn_contents).setBackgroundDrawable(UiUtil.createStateDrawable(mConfig.getThemeColor(), ContextCompat.getColor(this, R.color.white)));
+//            ((TextView) findViewById(R.id.btn_contents)).setTextColor(mConfig.getThemeColor());
+            ((TextView) findViewById(R.id.btn_contents)).setTextColor(UiUtil.getColorList(ContextCompat.getColor(this, R.color.white), mConfig.getThemeColor()));
+            ((TextView) findViewById(R.id.btn_highlights)).setTextColor(UiUtil.getColorList(ContextCompat.getColor(this, R.color.white), mConfig.getThemeColor()));
+            findViewById(R.id.btn_contents).setBackgroundDrawable(UiUtil.createStateDrawable(mConfig.getThemeColor(), ContextCompat.getColor(this, R.color.white)));
+            findViewById(R.id.btn_highlights).setBackgroundDrawable(UiUtil.createStateDrawable(mConfig.getThemeColor(), ContextCompat.getColor(this, R.color.white)));
         }
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
@@ -83,10 +88,17 @@ public class ContentHighlightActivity extends AppCompatActivity {
                 loadContentFragment();
             }
         });
+        findViewById(R.id.btn_highlights).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loadHighlightsFragment();
+            }
+        });
     }
 
     private void loadContentFragment() {
         findViewById(R.id.btn_contents).setSelected(true);
+        findViewById(R.id.btn_highlights).setSelected(false);
         TableOfContentFragment contentFrameLayout = TableOfContentFragment.newInstance(publication,
                 getIntent().getStringExtra(Constants.CHAPTER_SELECTED),
                 getIntent().getStringExtra(Constants.BOOK_TITLE));
@@ -97,6 +109,7 @@ public class ContentHighlightActivity extends AppCompatActivity {
 
     private void loadHighlightsFragment() {
         findViewById(R.id.btn_contents).setSelected(false);
+        findViewById(R.id.btn_highlights).setSelected(true);
         String bookId = getIntent().getStringExtra(FolioReader.EXTRA_BOOK_ID);
         String bookTitle = getIntent().getStringExtra(Constants.BOOK_TITLE);
         HighlightFragment highlightFragment = HighlightFragment.newInstance(bookId, bookTitle);
